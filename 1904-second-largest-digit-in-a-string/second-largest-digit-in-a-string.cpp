07baa0b6325d24eb41be6dc1c969c4b5f44a760d
sahilkamble002 <kamblesahil358358@gmail.com>
2026-02-08 23:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (68.19%) - LeetSync

---
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        int largest = -1;
+        int second_largest = -1;
+
+        for(char ch : s){
+            if (isdigit(ch)){
+                int num = ch - '0';
+            if(num > largest){
+                second_largest = largest;
+                largest = num;
+            }
+            else if(num > second_largest && num < largest){
+                second_largest = num;
+            }
+            }
+        }
+        return second_largest;
+    }
+};
