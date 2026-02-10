d02500d1508cbe7ee500ed21750807bbcf850d55
sahilkamble002 <kamblesahil358358@gmail.com>
2026-02-10 21:41:16 +0530

    Time: 1904 ms (60.17%) | Memory: 530.5 MB (27.88%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -4,10 +4,11 @@ public:
 
         int n = nums.size();
         int maxlength = 0;
-
+        //triying all possible starting points;
         for(int i = 0; i < n; i++){
             unordered_set<int> even, odd;
 
+            //starting form all possible points 
             for(int j =i; j < n; j++){
                 if(nums[j] % 2 == 0){
                     even.insert(nums[j]);
@@ -16,6 +17,7 @@ public:
                     odd.insert(nums[j]);
                 }
 
+                //checking is balanced or not
                 if(even.size() == odd.size()){
                     maxlength = max(maxlength, j-i+1);
                 }
