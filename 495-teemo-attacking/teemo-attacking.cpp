cb6c882423efc92872e959657729c51574f276f0
sahilkamble002 <kamblesahil358358@gmail.com>
2026-03-06 00:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (93.46%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+     int n = timeSeries.size();
+     int ans =0;
+     for(int i =0; i<n-1;i++){
+        ans+= min(timeSeries[i+1]- timeSeries[i], duration);
+     }   
+     return ans + duration;
+    }
+};
