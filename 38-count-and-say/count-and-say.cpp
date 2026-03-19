5e0a07616e3e6c5ec3aa42519c396e40412cfc3c
sahilkamble002 <kamblesahil358358@gmail.com>
2026-03-20 02:17:24 +0530

    Time: 3 ms (84.00%) | Memory: 9.7 MB (80.57%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+            string result = "1";
+
+    for (int i = 2; i <= n; i++) {
+        string temp = "";
+        int count = 1;
+
+        for (int j = 1; j < result.size(); j++) {
+            if (result[j] == result[j - 1]) {
+                count++;
+            } else {
+                temp += to_string(count);
+                temp += result[j - 1];
+                count = 1;
+            }
+        }
+
+        // handle last group
+        temp += to_string(count);
+        temp += result.back();
+
+        result = temp;
+    }
+
+    return result;
+    }
+};
