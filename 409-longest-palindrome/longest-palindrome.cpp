d9d518b3f7801bee0a1abd924dc0092fc511184c
sahilkamble002 <kamblesahil358358@gmail.com>
2026-03-24 00:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (38.12%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+            unordered_map<char, int> freq;
+    
+    for (char c : s) freq[c]++;
+    
+    int length = 0;
+    bool hasOdd = false;
+    
+    for (auto &p : freq) {
+        int count = p.second;
+        
+        length += (count / 2) * 2;
+        
+        if (count % 2 == 1) {
+            hasOdd = true;
+        }
+    }
+    
+    if (hasOdd) length += 1;
+    
+    return length;
+    }
+};
