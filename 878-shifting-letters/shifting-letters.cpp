1081431347d06a69503e0e15879796b676395318
sahilkamble002 <kamblesahil358358@gmail.com>
2026-03-30 15:47:11 +0530

    Time: 0 ms (100.00%) | Memory: 71.5 MB (86.63%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.size();
+    long long totalShift = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+        totalShift = (totalShift + shifts[i]) % 26;
+
+        // shift character
+        s[i] = ( (s[i] - 'a' + totalShift) % 26 ) + 'a';
+    }
+
+    return s;
+    }
+};
